8b8f2446f074cd0d2dc707a2f77843f3a3a3073f
Francesco <fstraps@gmail.com>
2021-05-29 12:08:05 +0200

    Test commit

--- pages/index.vue
+++ pages/index.vue
@@ -10,15 +10,27 @@
         sizes="xs:200px md:500px lg:1024"
       />
       <figcaption class="text-center text-gray-600 text-xs mt-3">
-        Photo by <a href="https://unsplash.com/@kydroon" target="_blank" class="underline">Kurt Cotoaga</a> on <a href="https://unsplash.com/s/photos/mountain" target="_blank" class="underline">Unsplash</a>
+        Photo by
+        <a
+          href="https://unsplash.com/@kydroon"
+          target="_blank"
+          class="underline"
+          >Kurt Cotoaga</a
+        >
+        on
+        <a
+          href="https://unsplash.com/s/photos/mountain"
+          target="_blank"
+          class="underline"
+          >Unsplash</a
+        >
       </figcaption>
     </figure>
   </main>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
 
 <style scoped>
