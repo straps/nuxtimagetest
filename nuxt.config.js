ea8245a0b9fb87e56685fba3022575854130036f
Francesco <fstraps@gmail.com>
2021-05-29 12:23:57 +0200

    Netlify

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,20 +1,19 @@
 export default {
   components: true,
+  target: "static",
   head: {
-    title: 'Nuxt Image Example',
+    title: "Nuxt Image Example",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ]
   },
-  modules: [
-    '@nuxt/image'
-  ],
+  modules: ["@nuxt/image"],
   image: {
-    domains: ['https://images.unsplash.com', 'https://source.unsplash.com']
+    domains: ["https://images.unsplash.com", "https://source.unsplash.com"]
   }
-}
+};
